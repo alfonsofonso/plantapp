63905747774cfe145f9ec9116cd80681f773e27b
alf <alfonsofonso@gmail.com>
2025-01-22 15:15:54 +0100

    ojeto

---
+++ arbol.js
@@ -0,0 +1,49 @@
+var arbol= {
+    biometric: {
+        nombre:"Jose",
+        altura:170,
+        peso:60,
+        edad
+    },
+    entorno: {
+        localizacion,
+        temperatura,
+        presion,
+        viajes: [
+            "cairo",
+            "tenoctiklan",
+            "stonehenge",
+            "ibiza"
+        ],
+    },
+    objetivos:{
+        relax,
+        dormir,
+        concentracion,
+
+    },
+    teorias:{
+        solfeggio,
+        shumman,
+        chacras,
+        etc
+    },
+
+
+}
+
+var factores={
+    notaBase:"altura",
+    numNotas:"nivel",
+    escala:"objetivos",
+    volumen:"volumen + velocidad",
+    tesitura:"nivel",
+    silencios:"hora",
+
+    volRuido:"limpieza"
+}
+
+function sonificaNombre(sustan){
+    sustantivo = sustan || "Jose";
+    
+}
