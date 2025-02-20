a67b2eb57013694d1154cb5b3983dc0dbd1f1177
mrHumildad <nghizzah@gmail.com>
2025-02-20 15:30:17 +0100

    milagrooooooooo aunque roto

--- ui.js
+++ ui.js
@@ -5,14 +5,16 @@ const myBar = document.getElementById("myBar");
 const playButt = document.getElementById('playButt')
 const myProgress = document.getElementById('myProgress')
 const userData = document.getElementById("userdata")
-/* const text = document.getElementById("text")
- */const showText = document.getElementById("showText")
+const text = document.getElementById("text")
+/* const showText = document.getElementById("showText") */
 const input = document.getElementById("input")
 const levelLog = document.getElementById("levelLog");
 const mycanvas = document.getElementById("mycanvas")   //no anda
 
 let user = [];
 let i = 0;
+let data = null;
+let welcome = true;
 
 function barAnimation() {
   console.log('baranimation')
@@ -24,16 +26,15 @@ function barAnimation() {
       //console.log(width)
       if (width >= 1000) {
         playButt.style.display="block";
-        if (nivel==-1) {
-          nivel = 0;
+        if (welcome) {
+          welcome = false  // repetido?
+          welcomeAlert();
+        } else if (levels.length > nivel-1) {// que quiere decir esto? SI QUEDAN NIVELES EN ARRAY
+          levelAlert();
         }
         clearInterval(id);
         i = 0;
         width = 1;
-        if (levels.length > nivel-1) {// que quiere decir esto? SI QUEDAN NIVELES EN ARRAY
-          levelAlert()
-        }
-        //return ;// y este return? SALES DEL IF LA BARRA ESTA LLENA
       } else {// y esto? LA BARRA SIGUE LLENANDOSE
         width += barSpeed;
         myBar.style.width = width/10 + "%";
@@ -67,6 +68,9 @@ function addLog(lvl, value){
 
 const levelUP = () => {
   const newValue = levels[nivel].value();
+  if (!newValue) {
+    return ;
+  }
   user.push(newValue)
   console.log('levelUP to ', nivel+1, user)
   console.log('value of input at level ', nivel, ' : ', newValue);
@@ -93,9 +97,25 @@ const levelAlert = () => {
  /*  mycanvas.style.opacity = 0.5; */
   /* playButt.onclick = levelUP; */
 }
+const welcomeAlert = () => {
+  console.log('welcome from level ', nivel);
+  const wellcomeText = user[1]
+  ? 'Wellcome back ' + user[1] + ' you can restart you experience from level ' + nivel
+  : "Welcome! The sound is the same for everyone. Progress to personalize it based on your unique vibration."
+  text.innerText = wellcomeText;
+  text.style.display = 'block';
+  playButt.disabled = false;
+  welcome = false;
+ /*  mycanvas.style.opacity = 0.5; */
+  /* playButt.onclick = levelUP; */
+}
 
 const playClick = () => {
-  if (nivel === -1) {
+  if (welcome) {
+    
+  }
+  welcome = false;
+  if (nivel === 0) {
     initHeal()
   } else {
     levelUP()
@@ -103,18 +123,29 @@ const playClick = () => {
 }
 
 const loadGame = () => {
-  const data = JSON.parse(localStorage.getItem("data"));
-  console.log("aqui nos quedamos", data);
+  data = JSON.parse(localStorage.getItem("data"));
   so = data.so;
   user = data.user;
   nivel = user.length - 1;
   levelLog.innerHTML = nivel;
-  for (let u = 0; u < user.length; u++) {
+  for (let u = 0; u < user.length; u++) { //populate userInfo
     const log = user[u];
     if (log !== null) {
       addLog(u, log)
     }
   };
+};
+
+function checkLocalStorage() {
+  if (localStorage.getItem('data') !== null) {
+      console.log("'data' key exists in localStorage.");
+      loadGame();
+      return true;
+  } else {
+      console.log("'data' key is not present in localStorage.");
+      initHeal()
+      return false;
+  }
 }
 
 onload=function(){
@@ -122,13 +153,8 @@ onload=function(){
   context.resume();
 	clock.tempo=120;//el tempo en lissajousJS
   barAnimation();
-  info()// un console log
-  console.log(this.localStorage);
-  if (this.localStorage.length === 0) {
-    console.log('new session');
-  } else {
-    loadGame()
-  }
+  info();// un console log
+  checkLocalStorage();
 }
 
 const resetLocalStorage = () => {
